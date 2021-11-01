ddac007992cc449a67d437f6277ee05d30385a65
ipakey <bornagainyfke@gmai.com>
2021-11-01 14:03:12 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header scss</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Kranky&family=Mulish:ital,wght@1,300&family=Spectral:ital,wght@1,300&family=The+Girl+Next+Door&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./scss/dist/style.css">
+
+</head>
+<body>
+    <header>
+        <h1>hello my dear</h1>
+        <button>Hello</button>
+    </header>
+    <div class="contact">
+        <button>Submit</button>
+        <div class="info">
+            <h1>Our contact info </h1>
+            <p>This is our info</p>
+        </div>
+    </div>
+
+</body>
+</html>
